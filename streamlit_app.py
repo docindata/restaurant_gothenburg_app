2a11cd8163f9b92d2182eec3ceb2a7ce0c091c95
docindata <92650082+docindata@users.noreply.github.com>
2025-01-29 12:01:03 +0000

    j

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,21 @@ dropbox_url = st.secrets["dropbox_url"]
 # Fetch the file content
 response = requests.get(dropbox_url)
 
+@st.cache_data # Decorator to save data as cache
+def load_data(csv_path):
+    df = pd.read_csv(csv_path)
+    df = df.reset_index(drop=True)
+    return df
+
+
 # Check if the request was successful
 if response.status_code == 200:
     print("File fetched successfully!")
     
     # Convert the content into a DataFrame
     csv_data = StringIO(response.text)
-    df = pd.read_csv(csv_data).loc[:,["title", "rating", "reviews", "type"]]
+
+    df = load_data(csv_data).loc[:,["title", "rating", "reviews", "type", "address"]]
     
     
     # Display the DataFrame
@@ -22,7 +30,11 @@ if response.status_code == 200:
 else:
     print(f"Failed to fetch file. Status code: {response.status_code}")
 
+# Display Restaurants with Styled Cards
+st.write("### What should we eat?")
 
-st.dataframe(df)
+st.sidebar.header("🔎 Search & Filters")
+search_query = st.sidebar.text_input("Search for a restaurant")
+selected_type = st.sidebar.multiselect("Filter by Type", options=df['type'].unique())
 
-print(df)
+st.write(df)
